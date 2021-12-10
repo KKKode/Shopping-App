1473c7388ad3b096dd0e8f636723dffb508d8210
KKKode <89744365+KKKode@users.noreply.github.com>
2021-12-10 17:57:54 +0530

    Update Nav.js

--- src/Component/ShoppingWeb/Nav.js
+++ src/Component/ShoppingWeb/Nav.js
@@ -22,29 +22,9 @@ const Nav = () => {
         </li>
         <li className="nav-item">
           <a className="nav-link" aria-current="page" href="#">My Cart</a>
-        </li>
-        {/* <li className="nav-item">
-          <a className="nav-link" href="#">Link</a>
-        </li> */}
-        {/* <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-            <li><a className="dropdown-item" href="#">Action</a></li>
-            <li><a className="dropdown-item" href="#">Another action</a></li>
-            <li><hr className="dropdown-divider"></li>
-            <li><a className="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link disabled">Disabled</a>
-        </li> */}
+        </li>             
       </ul>
       <form className="d-flex">
-      {/* <li className="nav-item">
-          <a className="nav-link" aria-current="page" href="#">Cart</a>
-        </li> */}
         <input className="form-control me-2" type="search" placeholder="Hey! Have a Look..." aria-label="Search"/>
         <button className="btn btn-outline-success" type="submit">Search</button>
       </form>
